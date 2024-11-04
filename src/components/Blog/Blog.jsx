eb18089abf0376ee7fc24d86ce5e58f7d92b4582
NishatRealm <nowrojnishat@gmail.com>
2024-11-05 02:11:59 +0600

    displayed data and hashtags

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,14 +1,33 @@
 
 import PropTypes from 'prop-types';
 const Blog = ({ blog }) => {
-    console.log(blog);
+    const{title,cover,author,author_img,posted_date,reading_time,hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex gap-4'>
+                    <img className="rounded-b-full w-14 mt-2" src={author_img} alt="" />
+                    <div>
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p className='text-blue-700'>
+                {
+                    hashtags.map((hash,idx) => <span key={idx}><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
-Blog.PropTypes ={
+Blog.propTypes ={
     blog : PropTypes.object.isRequired
 }
 export default Blog;
